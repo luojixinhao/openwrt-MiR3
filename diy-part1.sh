b20f4e790e424872c794b003983880531fdc113f
夏子草 <lx_2010@qq.com>
2024-05-28 19:44:49 +0800

    Update diy-part1.sh
    取消AdGuardHome

--- diy-part1.sh
+++ diy-part1.sh
@@ -16,5 +16,6 @@
 # Add a feed source
 #echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
-echo 'src-git AdGuardHome https://github.com/AdguardTeam/AdGuardHome >>feeds.conf.default
 echo 'src-git passwall2 https://github.com/xiaorouji/openwrt-passwall2' >>feeds.conf.default
+#echo 'src-git AdGuardHome https://github.com/AdguardTeam/AdGuardHome >>feeds.conf.default
+
